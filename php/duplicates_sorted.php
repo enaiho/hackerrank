05990d011d64c581ad492cda755ca1888daa7130
Michael Osahon <osahonmichael@yahoo.com>
2021-03-06 07:23:27 +0100

    duplicate

--- php/duplicates_sorted.php
+++ php/duplicates_sorted.php
@@ -1,9 +1,30 @@
 <?php
 
+function removeDuplicate($arr){
 
-function removeDuplicate(){
-	
+	$index = 0;
+	$pointer = 0;
+
+
+	foreach ($arr as $key => $value) {
+		
+		
+		if( $pointer !=  $value){
+
+			$arr[$index] = $value;
+			$index++;
+
+		}
+
+		$pointer = $value;
+		
+	}
+
+	return count(array_slice($arr, 0, $index));
+
 }
+
+print_r(removeDuplicate([1, 1, 1, 3, 5, 5, 7]));
 
 
 ?>
